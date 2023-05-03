da352d3f708525b0eff496e4a7285a178c574cf8
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-05-04 00:22:27 +0300

    change url

--- src/features/categories/categoryAction.js
+++ src/features/categories/categoryAction.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from '../../api/axios'
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getAllCategories = createAsyncThunk('categories/getAll', async ( thunkAPI) => {
     try {
-      const response = await axios.get('https://food-delivery-99a8.onrender.com/categories',
+      const response = await axios.get('/categories',
         {
           headers: {
             'Content-Type': 'application/json',
